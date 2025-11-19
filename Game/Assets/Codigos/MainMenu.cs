fcb4826c2ea316e326ecce4fa02049cc7caf7164
Jesus Hoppe <gamestarx777@gmail.com>
2025-11-19 09:09:06 -0500

    Video

--- Game/Assets/Codigos/MainMenu.cs
+++ Game/Assets/Codigos/MainMenu.cs
@@ -1,23 +1,58 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-   public void PlayGame()
+    [Header("Scene Settings")]
+    [SerializeField] private string gameSceneName = "Game";   // La escena que se carga al presionar Play
+    [SerializeField] private string autoSceneName = "Game";   // La escena a cargar automáticamente
+    [SerializeField] private float autoLoadDelay = 0f;        // Tiempo antes de cargar automáticamente (0 = desactivado)
+
+    private void Start()
     {
-     SceneManager.LoadScene("Game");
+        // Si autoLoadDelay es mayor a 0, empezamos la cuenta regresiva
+        if (autoLoadDelay > 0f)
+        {
+            StartCoroutine(AutoLoadSceneRoutine());
+        }
     }
 
+    // ---------------------------------------
+    //          BOTÓN PLAY
+    // ---------------------------------------
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(gameSceneName);
+    }
+
+    // ---------------------------------------
+    //       CAMBIAR A OTRA ESCENA MANUAL
+    // ---------------------------------------
+    public void LoadNextScene()
+    {
+        SceneManager.LoadScene(autoSceneName);
+    }
+
+    // ---------------------------------------
+    //     CAMBIAR DE ESCENA AUTOMÁTICAMENTE
+    // ---------------------------------------
+    private IEnumerator AutoLoadSceneRoutine()
+    {
+        yield return new WaitForSeconds(autoLoadDelay);
+        SceneManager.LoadScene(autoSceneName);
+    }
+
+    // ---------------------------------------
+    //               SALIR DEL JUEGO
+    // ---------------------------------------
     public void QuitGame()
     {
 #if UNITY_EDITOR
         UnityEditor.EditorApplication.isPlaying = false;
 #else
-    Application.Quit();
+        Application.Quit();
 #endif
     }
-
-
 }
+
